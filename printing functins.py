64af91ac835efb42159e282bce37ecdb6bfc8a1d
Basilshaman <basilshaman179@gmail.com>
2024-10-17 14:07:34 +0530

    Add files via upload

---
+++ printing functins.py
@@ -0,0 +1,40 @@
+veriable=[1,2,3,4,5,6,7,8]
+type(veriable)
+print(veriable)
+print(veriable[0])
+print(veriable[2])
+print(veriable[1::2])
+print(veriable[:-2:2])
+for number in veriable:
+    print(number)
+players=['gavi','pedri','balde','yamal']
+type(players)
+print(players)
+for names in players:
+    print(names)
+for number in range(1,100):
+    print(number)
+for names in range(1,1000):
+    print(names)
+for value in range(100,500,5):
+        print(value)
+for number in range(500,100,-1):
+    print(number)
+for number in range(1,100,8):
+    print(number)
+for number in range(0,160,8):
+    print(number)
+for number in range(0,8):
+     print(veriable[number])
+for number in range(0,len(veriable)):
+     print(veriable[number])
+for number in range(0,len(players)):
+     print(players[number])
+data=['name',1,1.2,1+3j,None,False]
+type(data)
+print(data[-1:-6:-1])
+for value in range(5,-1,-1):
+    print(data[value])
+for value in range(len(data)-1,-1,-1):
+    print(data[value])
+
