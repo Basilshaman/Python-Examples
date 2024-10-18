3145886873f1e05f97ee2d458ead4e2927f7de86
Basilshaman <basilshaman179@gmail.com>
2024-10-18 14:09:53 +0530

    Add files via upload

---
+++ printing list&character.py
@@ -0,0 +1,46 @@
+list1=[1,2,3,4,5,6,7]
+type(list1)
+for number in range(0,7,1):
+    print(number)
+for number in range(0,len(list1)):
+    print(list1[number]) 
+    
+for number in list1:   
+    if number%2==0:
+        print(number) 
+for number in range(0,100):
+    if number%2==1:
+        print(number)
+for number in range(50,501):
+    if number%2==0:
+        print(number)
+for number in range(50,100):
+    if number%2==0:
+        print(number+11)
+item=['toy','book','pen','meal']
+for list1 in item:
+    print(list1)
+item=['toy','book','pen','meal']
+for list1 in item:
+      print(list1[::-1])
+for list1 in range(0,4):
+     print(item[list1][::-1])
+'Basilshaman'
+for name in 'Basilshaman':
+    print(name)
+c='Bellingham'
+for name in c:
+    print(name)
+for name in c:
+    if name.isupper():
+        print(name)
+for name in c:
+    if name.islower():
+        print(name)
+
+
+
+
+
+
+
