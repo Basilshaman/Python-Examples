1c27dce8f768c7863c15bba0d658e58c78c6e835
Basilshaman <basilshaman179@gmail.com>
2025-02-11 13:51:42 +0530

    Add files via upload

---
+++ basic.sql
@@ -0,0 +1,12 @@
+create database basi;
+use basi;
+create table adress(
+name char(15),
+age int,
+id int);
+select* from adress;
+insert into adress values('dg',20,5003),('benny',22,5001),('tony',21,5004);
+select*from adress;
+
+
+
